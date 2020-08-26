8b11d30bfbfd59e6ebe735205b5eac046cac341d
Tony Luo <tonyluozn@gmail.com>
2020-08-26 21:19:44 +0800

    Obtain wallet address and store it in the states

--- src/App.js
+++ src/App.js
@@ -7,19 +7,38 @@ import storehash from './storehash';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Table, Button, Form, Row,Col} from 'react-bootstrap';
 
+
+
+
 class App extends Component {
  
-    state = {
-      ipfsHash:null,
-      verified:false,
-      buffer:'',
-      ethAddress:'',
-      blockNumber:'',
-      transactionHash:'',
-      gasUsed:'',
-      txReceipt: ''   
+  constructor() {
+    super();
+    //bring in user's metamask account address
+    this.getWalletAddress();
+  }
+
+  state = {
+    ipfsHash:null,
+    verified:true,
+    buffer:'',
+    ethAddress:'',
+    blockNumber:'',
+    transactionHash:'',
+    gasUsed:'',
+    txReceipt: '',
+    walletAddress:'' 
+  };  
+
+  getWalletAddress = async() =>{
+    const accounts =  await web3.eth.getAccounts();
+    console.log('Metamask account: ' + accounts[0]);
+    this.state = {
+      walletAddress: accounts[0]
     };
-captureFile =(event) => {
+  }
+
+  captureFile =(event) => {
         event.stopPropagation()
         event.preventDefault()
         const file = event.target.files[0]
@@ -27,53 +46,49 @@ captureFile =(event) => {
         reader.readAsArrayBuffer(file)
         reader.onloadend = () => this.convertToBuffer(reader)    
       };
- convertToBuffer = async(reader) => {
+  convertToBuffer = async(reader) => {
       //file is converted to a buffer for upload to IPFS
         const buffer = await Buffer.from(reader.result);
       //set this buffer -using es6 syntax
         this.setState({buffer});
     };
     
-onClick = async () => {
-try{
-        this.setState({blockNumber:"waiting.."});
-        this.setState({gasUsed:"waiting..."});
-//get Transaction Receipt in console on click
-//See: https://web3js.readthedocs.io/en/1.0/web3-eth.html#gettransactionreceipt
-await web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txReceipt)=>{
-          console.log(err,txReceipt);
-          this.setState({txReceipt});
-        }); //await for getTransactionReceipt
-await this.setState({blockNumber: this.state.txReceipt.blockNumber});
-        await this.setState({gasUsed: this.state.txReceipt.gasUsed});    
-      } //try
-    catch(error){
-        console.log(error);
-      } //catch
+  onClick = async () => {
+    try{
+          this.setState({blockNumber:"waiting.."});
+          this.setState({gasUsed:"waiting..."});
+    //get Transaction Receipt in console on click
+    //See: https://web3js.readthedocs.io/en/1.0/web3-eth.html#gettransactionreceipt
+    await web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txReceipt)=>{
+            console.log(err,txReceipt);
+            this.setState({txReceipt});
+          }); //await for getTransactionReceipt
+    await this.setState({blockNumber: this.state.txReceipt.blockNumber});
+          await this.setState({gasUsed: this.state.txReceipt.gasUsed});    
+        } //try
+      catch(error){
+          console.log(error);
+        } //catch
   } //onClick
-onSubmit = async (event) => {
-      event.preventDefault();
-     //bring in user's metamask account address
-      const accounts = await web3.eth.getAccounts();
-     
-      console.log('Sending from Metamask account: ' + accounts[0]);
+  onSubmit = async (event) => {
+    event.preventDefault();
     //obtain contract address from storehash.js
-      const ethAddress= await storehash.options.address;
-      this.setState({ethAddress});
+    const ethAddress= await storehash.options.address;
+    this.setState({ethAddress});
     //save document to IPFS,return its hash#, and set hash# to state
     //https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/FILES.md#add 
-      await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
         console.log(err,ipfsHash);
         //setState by setting ipfsHash to ipfsHash[0].hash 
         this.setState({ ipfsHash:ipfsHash[0].hash });
-   // call Ethereum contract method "sendHash" and .send IPFS hash to etheruem contract 
-  //return the transaction hash from the ethereum contract
- //see, this https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-send
+     // call Ethereum contract method "sendHash" and .send IPFS hash to etheruem contract 
+    //return the transaction hash from the ethereum contract
+    //see, this https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-send
       // if the user has the tokens, 
-
+      
       if(this.verified)  {
         storehash.methods.sendHash(this.state.ipfsHash).send({
-          from: accounts[0] 
+          from: this.walletAddress
         }, (error, transactionHash) => {
           console.log(transactionHash);
           this.setState({transactionHash});
@@ -81,7 +96,7 @@ onSubmit = async (event) => {
       }
       }) //await ipfs.add 
     }; //onSubmit
-  
+
 
     // for any user who has metamask, send the ERC-20 tokens to the account.
     getToken = async () => {}
--- src/web3.js
+++ src/web3.js
@@ -2,5 +2,7 @@
 //1.0 lets us use async and await instead of promises
 import Web3 from 'web3';
 
+
 const web3 = new Web3(window.web3.currentProvider);
+
 export default web3;
