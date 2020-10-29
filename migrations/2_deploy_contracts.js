a01e75aac73e23a6b3aeb5e9db46174485306f50
Tony Luo <tonyluozn@gmail.com>
2020-10-29 11:04:29 +0800

    Deploy NUHealthToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var StoreHash = artifacts.require("./StoreHash.sol");
+var NUHealthToken = artifacts.require("./NUHealthToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(StoreHash);
+};
+
+module.exports = function(deployer) {
+  deployer.deploy(NUHealthToken);
 };
