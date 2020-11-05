a534c89479719d0c005553f15d28de9f6d4fbd5e
Tony Luo <tonyluozn@gmail.com>
2020-11-05 10:15:21 +0800

    Replace old Contract

--- client/src/storehash.js
+++ client/src/storehash.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
-import { storehash } from './contracts/Storehash.json';
+import storehash  from './contracts/StoreHash.json';
 //access our local copy to contract deployed on rinkeby testnet
 //use your own contract address
-const networkId = await web3.eth.net.getId();
+const networkId = async()=>{await web3.eth.net.getId();}
 const deployedNetwork = storehash.networks[networkId];
 const Storehash = new web3.eth.Contract(
 	storehash.abi,
