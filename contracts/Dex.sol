555feb19bf31d69b014f541f26486bb70e067233
Tony Luo <tonyluozn@gmail.com>
2021-03-12 10:20:56 +0800

    contract sell function update

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -121,7 +121,7 @@ contract Dex {
         uint256 allowance = token.allowance(msg.sender, address(this));
         require(allowance >= amount, "Check the token allowance");
         token.transferFrom(msg.sender, address(this), amount);
-        msg.sender.transfer(amount);
+        //msg.sender.transfer(amount);
         emit SendBack(amount);   
     }
 
